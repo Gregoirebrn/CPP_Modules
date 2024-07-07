3e35045cc6a7a7177a6a570c8950e07bfd6d1b38
Grgoirebrn <gregoire2246@gmail.com>
2024-07-07 15:27:27 +0200

    added file ex01

---
+++ mod_0/ex01/includes/PhoneBook.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 15:26:35 by grebrune          #+#    #+#             */
+/*   Updated: 2024/07/07 15:26:35 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+# include <string>
+# include "contact.hpp"
+
+class PhoneBook {
+	public :
+		void	Creator(void);
+		void	new_contact(void);
+		void	search(void);
+	private :
+		Contact	friends[8];
+		int		index;
+		int		nbr_friends;
+};
+
+
+#endif
