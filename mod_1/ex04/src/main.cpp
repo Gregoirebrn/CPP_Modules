864f3558fed4ea7e561cd95480f0d09c88dae431
grebrune <grebrune@student.42lyon.fr>
2024-07-16 19:55:33 +0200

    to do

--- mod_1/ex04/src/main.cpp
+++ mod_1/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 16:44:46 by grebrune          #+#    #+#             */
-/*   Updated: 2024/07/15 17:20:59 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/07/16 17:12:07 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,30 @@
 #include <fstream>
 # include <string>
 
-
-int main(int ac, char **av)
+void	fill_fill_replace(std::string file_name, std::string format)
 {
-	std::ifstream in_file;
-	std::ofstream out_file;
-	std::string   format;
-	if (ac < 4 || ac > 4)
-		return std::cout << "Wrong parameters" << std::endl, 1;
-	in_file.open(av[1]);
-	std::ofstream outfile("out_file");
+	std::ifstream in_file(file_name);
+	std::ofstream outfile(file_name + ".replace");
+
 	if (in_file.is_open())
 	{
 		while (getline(in_file, format))
 		{
-			out_file << format << '\n';
+			outfile << format << '\n';
 		}
 		in_file.close();
+		outfile.close();
 	}
+}
+
+int main(int ac, char **av)
+{
+	std::ifstream in_file(av[1]);
+	std::string   format;
+
+	if (ac < 4 || ac > 4)
+		return std::cout << "Wrong parameters" << std::endl, 1;
+	if (!in_file.is_open())
+		std::cout << "Failed to open " << av[1] << std::endl;
+	fill_fill_replace(av[1], format);
 }
