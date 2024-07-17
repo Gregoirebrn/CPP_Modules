3a8fb0b09a3e854a184b17a635dde6c7cf45119b
grebrune <grebrune@student.42lyon.fr>
2024-07-17 16:16:28 +0200

    finished

--- mod_1/ex04/src/main.cpp
+++ mod_1/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 16:44:46 by grebrune          #+#    #+#             */
-/*   Updated: 2024/07/17 14:44:54 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/07/17 16:10:22 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,37 @@
 
 void	fill_fill_replace(std::ifstream& in_file, std::ofstream& outfile, std::string s1, std::string s2)
 {
-	std::string   format = NULL;
-	std::size_t   posi = 0;
+	std::string   format;
 
 	while (getline(in_file, format))
 	{
-		while (posi == s2.find(s1, posi))
+		std::size_t   posi = 0;
+		while ((posi = format.find(s1, posi)) != std::string::npos)
 		{
-			format = format.substr(posi) + s2 + format.substr(posi + s1.length());
+			format = format.substr(0 , posi) + s2 + format.substr(posi + s1.length());
+			posi += s2.length();
 		}
 		outfile << format << '\n';
 	}
-	in_file.close();
-	outfile.close();
 }
 
 int main(int ac, char **av)
 {
 	if (ac < 4 || ac > 4)
 		return std::cout << "Wrong parameters" << std::endl, 1;
+
 	std::ifstream in_file(av[1]);
+	if (!in_file.is_open())
+		 return std::cout << "Failed to open " << av[1] << std::endl, 1;
+
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	if (s1.empty() || s2.empty())
+		return std::cout << "A string can not be empty !" << std::endl, 1;
+
 	std::string fileName(av[1]);
 	fileName.append(".replace");
 	std::ofstream outfile(fileName.c_str(), std::ios_base::out);
-	std::string s1 = av[2];
-	std::string s2 = av[3];
 
-	if (!in_file.is_open())
-		std::cout << "Failed to open " << av[1] << std::endl;
 	fill_fill_replace(in_file, outfile, s1, s2);
 }
