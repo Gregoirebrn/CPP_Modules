ee10f40830a6d636f65f99a3b2c0eb5a26e94d36
grebrune <grebrune@student.42lyon.fr>
2024-07-25 14:53:11 +0200

    to finish & check

---
+++ mod_2/ex02/src/Fixed.cpp
@@ -0,0 +1,173 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/23 17:29:48 by grebrune          #+#    #+#             */
+/*   Updated: 2024/07/25 14:47:28 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _nbr(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int nbr)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_nbr = nbr << _nbr_bits;
+}
+
+Fixed::Fixed(const float nbr)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_nbr = roundf(nbr * (1 << _nbr_bits));
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fix) {
+	std::cout << "Copy constructor operator called" << std::endl;
+	this->_nbr = fix.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &fix) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this == &fix)
+		return (*this);
+	this->_nbr = fix.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits() const {
+	return (this->_nbr);
+}
+
+void Fixed::setRawBits(const int raw) {
+	this->_nbr = raw;
+}
+
+float Fixed::toFloat() const {
+	return _nbr / static_cast<float>((1 << _nbr_bits));
+}
+
+
+int Fixed::toInt() const {
+	return this->_nbr >> _nbr_bits;
+}
+
+std::ostream &operator<<(std::ostream &o, Fixed const &fix){
+	o << static_cast<float>(fix.toFloat());
+	return o;
+}
+
+bool Fixed::operator>(const Fixed &fix) const
+{
+	return this->_nbr > fix._nbr;
+}
+
+bool Fixed::operator<(const Fixed &fix) const
+{
+	return this->_nbr < fix._nbr;}
+
+bool Fixed::operator>=(const Fixed &fix) const
+{
+	return this->_nbr >= fix._nbr;
+}
+
+bool Fixed::operator<=(const Fixed &fix) const
+{
+	return this->_nbr <= fix._nbr;
+}
+
+bool Fixed::operator==(const Fixed &fix) const
+{
+	return this->_nbr == fix._nbr;
+}
+
+bool Fixed::operator!=(const Fixed &fix) const
+{
+	return this->_nbr != fix._nbr;
+}
+
+Fixed Fixed::operator+(const Fixed &fix) const
+{
+	Fixed out;
+
+	out.setRawBits(this->_nbr + fix._nbr);
+	return out;
+}
+
+Fixed Fixed::operator-(const Fixed &fix) const
+{
+	Fixed out;
+
+	out.setRawBits(this->_nbr - fix._nbr);
+	return out;}
+
+Fixed Fixed::operator*(const Fixed &fix) const
+{
+	Fixed out;
+
+	out.setRawBits(this->_nbr * fix._nbr);
+	return out;
+}
+
+Fixed Fixed::operator/(const Fixed &fix) const
+{
+	Fixed out;
+
+	out.setRawBits(this->_nbr / fix._nbr);
+	return out;}
+
+Fixed &Fixed::operator++(void)
+{
+	Fixed tmp;
+
+	tmp = *this;
+	++*this;
+	return tmp;
+}
+
+Fixed &Fixed::operator--(void)
+{
+	Fixed tmp;
+
+	tmp = *this;
+	--*this;
+	return tmp;
+}
+
+Fixed Fixed::operator++(int digit)
+{
+	this->_nbr++;
+	return *this;
+}
+
+Fixed Fixed::operator--(int digit)
+{
+	this->_nbr--;
+	return *this;
+}
+
+int Fixed::max(int a, int b) {
+	if (a < b)
+		return b;
+	return a;
+}
+
+Fixed& Fixed::min(Fixed a, int b) {
+	if (a < b)
+		return a;
+	return b;
+}
+
+int Fixed::max(const int a, const int b) {}
---
+++ mod_2/ex02/src/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 17:16:19 by grebrune          #+#    #+#             */
+/*   Updated: 2024/07/24 17:00:01 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
