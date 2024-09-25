9b0d579efbfb657ad3e67853abd4e55173cb4d2c
Grgoirebrn <gregoire2246@gmail.com>
2024-09-25 20:52:47 +0200

    all good & ready for push

--- mod_7/ex02/includes/Array.hpp
+++ mod_7/ex02/includes/Array.hpp
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 12:35:51 by grebrune          #+#    #+#             */
+/*   Updated: 2024/09/25 18:49:14 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
@@ -7,36 +18,39 @@
 template < typename T> class Array {
 private:
 	T *_array;
-	unsigned int _n;
+	size_t _n;
 public:
 	Array() : _array(NULL), _n(0) {};
 
-	Array(unsigned int n) : _array(new T[n]) , _n(n){};
+	Array(size_t n) : _array(new T[n]()) , _n(n){};
 
-	unsigned int size() const {return _n; };
+	~Array() { delete [] _array; }
 
-	T & operator[](unsigned int n) const {
-		if (_n < n)
-			throw ;
+	size_t size() const {return _n; };
+
+	T & operator[](size_t n) const {
+		if (_n <= n || _n == 0)
+			throw OutBond();
 		return (_array[n]);
 	}
 
+	Array(Array &copy) : _array(new T[copy._n]()) , _n(copy._n){}
+
 	Array & operator = (const Array &rhs) {
-		if (this == &rhs)
-			return *this;
-		if (this->_n != rhs._n) {
-			delete this->_array;
-			this->_array = Array(rhs._n);
-		}
-		for (unsigned int i = 0; i < this->_n; ++i) {
-			this->_array[i] = rhs._array[i];
+		if (this != &rhs) {
+			delete [] _array;
+			this->_n = rhs._n;
+			this->_array = new T[_n]();
+			for (size_t i = 0; i < this->_n; ++i) {
+				this->_array[i] = rhs._array[i];
+			}
 		}
 		return *this;
 	}
 	class OutBond : public std::exception {
 	public:
 		virtual const char* what() const throw() {
-			return (this->_n);
+			return ("Wrong index");
 		}
 	};
 };
--- mod_7/ex02/src/main.cpp
+++ mod_7/ex02/src/main.cpp
@@ -6,26 +6,242 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:05:22 by grebrune          #+#    #+#             */
-/*   Updated: 2024/09/23 17:32:37 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/09/25 18:50:11 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int main(void)
-{
-	std::cout << "----------------BEGIN---------------" << std::endl;
-	try {
-		Array<> * a = new int();
-		std::cout << "int *a = " << a;
-		std::cout << a[10];
-		*a = int(10);
-		delete a;
-	}
-	catch (std::exception &e)
+int	main() {
+
 	{
-		std::cout << "Error: Index is out of bounds" << e.what() << " is the max." << std::endl;
+		std::size_t size = 12;
+		Array<unsigned int> tab(size);
+		Array<unsigned int> tab2(size * 2);
+
+		tab = tab2;
+
 	}
-	std::cout << "----------------DISPLAY---------------" << std::endl;
-	std::cout << "----------------END---------------" << std::endl;
+//	return (0);
+	{
+		unsigned int		size = 12;
+		Array<std::string>	test(size);
+
+		std::cout << "size = " << test.size() << std::endl;
+		try {
+			for (unsigned int i = 0; i < test.size(); i++) {
+				test[i] = "hey";
+			}
+			std::cout << "11 = " << test[11] << std::endl;
+			test[11] = "salut";
+			std::cout << "11 = " << test[11] << std::endl;
+			for (unsigned int i = 0; i < test.size(); i++) {
+				std::cout << i << " = " << test[i] << std::endl;
+			}
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "-1 = " << test[-1] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "13 = " << test[13] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "12 = " << test[12] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+	}
+	{
+		unsigned int	size = 12;
+		Array<int>		test(size);
+
+		std::cout << "size = " << test.size() << std::endl;
+		try {
+			std::cout << "11 = " << test[11] << std::endl;
+			for (unsigned int i = 0; i < test.size() ; i++) {
+				test[i] = 42;
+			}
+			std::cout << "11 = " << test[11] << std::endl;
+			test[11] = 97;
+			std::cout << "11 = " << test[11] << std::endl;
+			for (unsigned int i = 0; i < test.size() ; i++) {
+				std::cout << i << " = " << test[i] << std::endl;
+			}
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "-1 = " << test[-1] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "13 = " << test[13] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+	}
+	{
+		unsigned int	size = 12;
+		Array<float>	test(size);
+
+		std::cout << "size = " << test.size() << std::endl;
+		try {
+			std::cout << "11 = " << test[11] << std::endl;
+			for (unsigned int i = 0; i < test.size() ; i++) {
+				test[i] = 42.42;
+			}
+			std::cout << "11 = " << test[11] << std::endl;
+			test[11] = 19.97;
+			std::cout << "11 = " << test[11] << std::endl;
+			for (unsigned int i = 0; i < test.size() ; i++) {
+				std::cout << i << " = " << test[i] << std::endl;
+			}
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "-1 = " << test[-1] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "13 = " << test[13] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+	}
+	{
+		Array<int>	test;
+
+		std::cout << "size = " << test.size() << std::endl;
+		try {
+			std::cout << "0 = " << test[0] << std::endl;
+			for (unsigned int i = 0; i < test.size() ; i++) {
+				test[i] = 42;
+			}
+			std::cout << "11 = " << test[11] << std::endl;
+			test[11] = 97;
+			std::cout << "11 = " << test[11] << std::endl;
+			for (unsigned int i = 0; i < test.size() ; i++) {
+				std::cout << i << " = " << test[i] << std::endl;
+			}
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "-1 = " << test[-1] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "13 = " << test[13] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+	}
+	{
+		unsigned int		size = 4;
+		Array<Array<int> >	test(size);
+
+		std::cout << "size = " << test.size() << std::endl;
+		try {
+			for (unsigned int i = 0; i < test.size(); ++i) {
+				std::cout << "size["<<i<<"] = " << test[1].size() << std::endl;
+			}
+			for (unsigned int i = 0; i < test.size(); ++i) {
+				for (unsigned int j = 0; j < test[i].size(); ++j) {
+					test[i][j] = i + j;
+				}
+			}
+			for (unsigned int i = 0; i < test.size(); ++i) {
+				for (unsigned int j = 0; j < test[i].size(); ++j) {
+					std::cout << "test["<<i<<"]["<<j<<"] = "<<test[i][j] << std::endl;
+				}
+			}
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "-1 = " << test[-1][0] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		try {
+			std::cout << "5 = " << test[13][0] << std::endl;
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+	}
+	{
+		Array<int>	*a = new Array<int>();
+		int			*b = new int();
+
+		std::cout << "*a is: " << &a << std::endl;
+		std::cout << "*a is: " << a << std::endl;
+		std::cout << "*b is: " << &b << std::endl;
+		std::cout << "*b is: " << b << std::endl;
+		delete a;
+		delete b;
+	}
+	{
+		Array<float>	test(12);
+		try {
+			std::cout << "size = " << test.size() << std::endl;
+			std::cout << "default test [11] = "  << test[11] << std::endl;
+			for  (unsigned int i = 0; i < test.size() ; i++) {
+				test[i] = 42.42;
+			}
+			std::cout << "1st init test [11] = " << test[11] << std::endl;
+			test[11] = 19.97;
+			std::cout << "2nd init test [11] = " << test[11] << std::endl;
+			for  (unsigned int i = 0; i < test.size() ; i++) {
+				std::cout<< "test ["<< i << "] = " << test[i] << std::endl;
+			}
+		}
+		catch (std::exception &error) {
+			std::cout << error.what() << std::endl;
+		}
+		Array<float>	cpy(test);
+		Array<float>	cpy2 = cpy;
+		for  (unsigned int i = 0; i < cpy.size() ; i++) {
+			std::cout << "cpy ["<< i << "] = " << cpy[i] << std::endl;
+		}
+		for  (unsigned int i = 0; i < cpy2.size() ; i++) {
+			std::cout << "cpy2 ["<< i << "] = " << cpy2[i] << std::endl;
+		}
+		std::cout << "modifying cpy2" << std::endl;
+		cpy2[3] = 12.54;
+		for  (unsigned int i = 0; i < test.size() ; i++) {
+			std::cout<< "test ["<< i << "] = " << test[i] << std::endl;
+		}
+		for  (unsigned int i = 0; i < cpy.size() ; i++) {
+			std::cout << "cpy ["<< i << "] = " << cpy[i] << std::endl;
+		}
+		for  (unsigned int i = 0; i < cpy2.size() ; i++) {
+			std::cout << "cpy2 ["<< i << "] = " << cpy2[i] << std::endl;
+		}
+	}
 }
