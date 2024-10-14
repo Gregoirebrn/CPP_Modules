d17e7dbff202f53a3d806ccd4209c933a5b12faf
grebrune <grebrune@student.42lyon.fr>
2024-10-14 20:45:16 +0200

    begin of insert working

--- mod_9/ex02/includes/PmergeMe.hpp
+++ mod_9/ex02/includes/PmergeMe.hpp
@@ -6,13 +6,15 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 13:58:22 by grebrune          #+#    #+#             */
-/*   Updated: 2024/10/12 19:41:32 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/10/14 15:51:17 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+#include <cstdlib>
+#include <iterator>
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -24,7 +26,7 @@ class PmergeMe {
 	std::vector< std::pair<int,int> >		_pair;
 	std::vector<int>		_vec;
 	std::vector<int>		_final;
-	std::vector<int>		_jacob;
+	int						_jacob_number;
 //	std::deque<int>			_dec;
 public:
 	~PmergeMe();
@@ -36,15 +38,17 @@ public:
 	void vec_algo(char **av);
 //	void deq_algo(char **av);
 
-	int jacobsthal(int n);
+	size_t jacobsthal(int n);
 	void print_pair_vec(std::vector<std::pair< int , int > > V);
 	void fill_vec();
 	void print_vec();
 	void print_all(std::vector<int> J);
 //	std::vector<int> fordJohnsonSortRecursive(std::vector<std::pair<int, int> >& pairs);
 
-	void recur(std::vector< std::pair<int,int> > input, int n);
+	void recur(std::vector< std::pair<int,int> > input);
 	static bool is_sort(std::vector< std::pair<int,int> > input);
+	int find_small(std::vector<std::pair<int, int> > haystack, int to_find);
+	std::vector<std::pair< int , int > >::iterator find_iter(std::vector<std::pair< int , int > > V, int i);
 };
 
 #endif
--- mod_9/ex02/src/PmergeMe.cpp
+++ mod_9/ex02/src/PmergeMe.cpp
@@ -6,12 +6,11 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 13:57:57 by grebrune          #+#    #+#             */
-/*   Updated: 2024/10/12 20:16:43 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/10/14 17:27:03 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdlib>
-#include <algorithm>
+#include <complex>
 #include "PmergeMe.hpp"
 
 PmergeMe::PmergeMe() {
@@ -33,7 +32,7 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &origin) {
 	return *this;
 }
 
-PmergeMe::PmergeMe(char **av) {
+PmergeMe::PmergeMe(char **av) : _jacob_number(0){
 	vec_algo(av);
 //	deque(av);
 }
@@ -86,37 +85,6 @@ void	PmergeMe::fill_vec() {
 //	print_all(sortedResult);
 }
 
-int PmergeMe::jacobsthal(int n) {
-	std::vector<int> J(n);
-	if (n < 1)
-		return 0;
-	if (n < 2)
-		return 1;
-	else {
-		J[0] = 0;
-		J[1] = 1;
-		for (int i = 2; i < n; ++i) {
-			J[i] = J[i - 1] + (2 * J[i - 2]);
-		}
-	}
-	for (size_t i = 0; i < J.size(); ++i) {
-		std::cout << "J[" << i << "] =" << J[i] << std::endl;
-	}
-	std::cout << "J de n = " << J[n - 1] << std::endl;
-	return (J[n - 1] + (2 * J[n - 2]));
-}
-
-void PmergeMe::vec_algo(char **av) {
-	int n = 0;
-	for (n = 0; av[n]; n++) {
-		check_str(av[n]);
-		_vec.push_back(std::atoi(av[n]));
-	}
-	fill_vec();
-	recur(_pair, 0);
-	print_all(_final);
-}
-
 void PmergeMe::print_all(std::vector<int> J) {
 	int i = 0;
 	std::cout << "RESULT of " << J.size() << " elements :" << std::endl;
@@ -126,9 +94,29 @@ void PmergeMe::print_all(std::vector<int> J) {
 	}
 }
 
-void	PmergeMe::recur(std::vector< std::pair<int,int> > input, int n) {
-	if (is_sort(input))
+void PmergeMe::vec_algo(char **av) {
+	int n = 0;
+	for (n = 0; av[n]; n++) {
+		check_str(av[n]);
+		_vec.push_back(std::atoi(av[n]));
+	}
+	fill_vec();
+	recur(_pair);
+	print_all(_final);
+}
+
+size_t PmergeMe::jacobsthal(int idx) {
+	if (idx == 0)
+		return 0;
+	return (static_cast<std::size_t>(std::pow(2, idx + 1)) + static_cast<std::size_t>(std::pow(-1, idx))) / 3;
+}
+
+void	PmergeMe::recur(std::vector< std::pair<int,int> > input) {
+	if (is_sort(input)) {
+		_final.push_back(input[0].first);
+		_final.push_back(input[0].second);
 		return ;
+	}
 	std::vector< std::pair<int,int> > to_next;
 	for (size_t i = 0; i < input.size() - 1; i += 2) {
 		std::pair<int, int> pair;
@@ -141,22 +129,79 @@ void	PmergeMe::recur(std::vector< std::pair<int,int> > input, int n) {
 		}
 		to_next.push_back(pair);
 	}
+	recur(to_next);
+	if (1 == to_next.size())
+		return ;
+
+	size_t dist = jacobsthal(++_jacob_number);
+	std::cout << "first Dist = " << dist << std::endl;
+	if (dist > _final.size())
+		dist = _final.size() - 1 ;
 	print_pair_vec(to_next);
-	int j = jacobsthal(n);
-	std::cout << "jacob = " << j << std::endl;
-	recur(to_next, n + 1);
-	if (_final.empty()) {
-		_final.push_back(to_next[0].first);
-		_final.push_back(to_next[0].second);
+	size_t last_jacob_number = jacobsthal(_jacob_number - 1);
+	std::cout << "size = " << to_next.size() << std::endl;
+	std::cout << "Jacob Number =" << _jacob_number << std::endl;
+	std::cout << "Last Jacob Number =" << last_jacob_number << std::endl;
+
+
+	while (dist >= last_jacob_number) {
+	std::cout << "Dist = " << dist << std::endl;
+		int i = find_small(to_next, _final[dist]);
+				std::cout << "7" << std::endl;
+//			print_all(_final);
+		if (i == -1) {
+			std::cout << "i  = " << i << "next first value " << to_next[i].first << std::endl;
+			i = find_small(to_next, _final[dist + 1]);
+			std::cout << "i  = " << i << "next first value " << to_next[i].first << std::endl;
+		}
+			std::cout << "i  = " << i << "next first value " << to_next[i].first << std::endl;
+		for(std::vector<int>::iterator it=_final.begin() + jacobsthal(_jacob_number); it!=_final.begin(); --it) {
+			std::cout << "iterator value = " << *it << std::endl;
+			if (*it > to_next[i].first && ( it == _final.begin() || *(it - 1) < to_next[i].first)) {
+				std::cout << "fTTTTTTTTTT = " << std::endl;
+				_final.insert(it, to_next[i].first);
+				break;
+			}
+				std::cout << "UUUUUUUUUUUUU = " << std::endl;
+			if ((it - 1) == _final.begin()) {
+				_final.insert((it - 1), to_next[i].first);
+				std::cout << "6" << std::endl;
+			print_all(_final);
+			std::cout << "frrrrrrrrrre = " << std::endl;
+//				to_next.erase(find_iter(to_next, i));
+				break;
+			}
+		}
+		if (dist == 0) {
+				std::cout << "5" << std::endl;
+			print_all(_final);
+			return;
+		}
+		dist--;
 	}
+	std::cout << "Final end of recur" << std::endl;
+}
+
+std::vector<std::pair< int , int > >::iterator PmergeMe::find_iter(std::vector<std::pair< int , int > > V, int i) {
+	int find = 0;
+	for (std::vector<std::pair< int , int > >::iterator it = V.begin(); it != V.end(); it++) {
+		if (find == i)
+			return it;
+		find++;
+	}
+	std::vector<std::pair< int , int > >::iterator it = V.end();
+	return it;
+}
+//			std::cout << "BBB" << std::endl;
+//		std::cout << "AAA" << std::endl;
+
+	//rest of elements
 //	else {
 //		for (std::vector<std::pair<int ,int > >::iterator it = to_next.begin(); it < to_next.end(); it++) {
 //			if (_final[j / 2] < it)
 //			_final.push_back(to_next[j].first);
 //		}
 //	}
-	return ;
-}
 
 bool PmergeMe::is_sort(std::vector<std::pair<int, int> > input) {
 	for (size_t i = 0; i < input.size() - 1; i++) {
@@ -164,5 +209,15 @@ bool PmergeMe::is_sort(std::vector<std::pair<int, int> > input) {
 			return false;
 	}
 	return true;
+}
+
+int PmergeMe::find_small(std::vector<std::pair<int, int> > haystack, int to_find) {
+	if (haystack[0].second == to_find)
+			return 0;
+	for (size_t it = 0; it < haystack.size(); it++) {
+		if (haystack[it].second == to_find)
+			return static_cast<int>(it);
+	}
+	return -1;
 }
 
